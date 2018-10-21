c367c1c6e8b529b31a526c11706678ac8cc1bae1
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-10-21 12:00:43 +0600

    Card componet created

---
+++ src/App/Card/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+ const Card = () => {
+  return (
+    <div>
+        <img alt="robot" src="https://robohash.org/test?200*200"/>
+        <div>
+             <h2>the Iron man</h2>
+             <p>ironMan@gmail.com</p>
+        </div>
+    </div>
+  )
+}
+
+export default Card
