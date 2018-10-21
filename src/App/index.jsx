14532826c5c5410c2252d7ea05dd92d51face363
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-10-21 11:38:28 +0600

    refactoring init react

---
+++ src/App/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+export class App extends Component {
+ 
+  render() {
+    return (
+      <div>
+          <h1 style={{tc:'center'}}>Parent App</h1>
+        
+      </div>
+    )
+  }
+}
+
+
+
+export default App
