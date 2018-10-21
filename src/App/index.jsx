a1141d4d6ee9b4760b7d19713ae91ce5f58b1551
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-10-21 15:27:24 +0600

    searchengine implement done

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -2,15 +2,34 @@ import React, { Component } from 'react'
 import {robots} from '../App/API';
 import SearchBox from '../App/searchBox'
 import CardList from '../App/CardList';
+import { SSL_OP_NETSCAPE_REUSE_CIPHER_CHANGE_BUG } from 'constants';
+
+
 export class App extends Component {
+
+    state = {
+        robots:robots,
+        searchField:''
+    }
+
+    onSearchChange=(event)=>{
+    
+        this.setState({
+            searchField:event.target.value
+        })
+    }
  
   render() {
-    return (
 
-      <div className='container'>
-          <h1 style={{tc:'center'}}>Parent App</h1>
-          <SearchBox/>
-          <CardList robots={robots}/>
+    const filterRobots = this.state.robots.filter(robot => {
+        return robot.name.toLocaleLowerCase().includes(this.state.searchField.toLocaleLowerCase())
+    })
+    return (
+ 
+      <div className='tc'>
+          <h1 style={{tc:'center'}}>Realtime search app</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <CardList robots={filterRobots}/>
         
       </div>
     )
