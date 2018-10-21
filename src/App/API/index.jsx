da556aaa9641ad70c7e423b688c88fe43559a71a
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-10-21 12:18:31 +0600

    api sample data creaded

---
+++ src/App/API/index.jsx
@@ -0,0 +1,38 @@
+export const robots = [
+    {
+        name:'Tanvir Hasan',
+        username:'Tnavir',
+        email:'tanvir@gmail.com'
+
+    },
+    {
+        name:'Emran Hasan',
+        username:'Emaran',
+        email:'Emran@gmail.com'
+
+    },
+    {
+        name:'Iron Man',
+        username:'iRON',
+        email:'iron@gmail.com'
+
+    },
+    {
+        name:'The Batman',
+        username:'Batman',
+        email:'batman@gmail.com'
+
+    },
+    {
+        name:'SpiderMan',
+        username:'Spider',
+        email:'spider@gmail.com'
+
+    },
+    {
+        name:'the Robo',
+        username:'Robot',
+        email:'Robo@gmail.com'
+
+    },
+]
--- src/App/Card/index.jsx
+++ src/App/Card/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
  const Card = () => {
   return (
     <div className='bg-light-green dib br3 pa3 ma2 grow'>
-        <img alt="robot" src="https://robohash.org/test?200*200"/>
+        <img alt="robot" src="https://robohash.org/test?200x200"/>
         <div className='tc'>
              <h2>the Iron man</h2>
              <p>ironMan@gmail.com</p>
